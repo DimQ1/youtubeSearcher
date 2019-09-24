831a72eea6c58832f7a83a7aa10f7f4c40b024a6
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-09-24 11:34:26 +0300

    removed not the necessary link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
