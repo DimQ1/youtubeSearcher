7c8791ae181219869151129dcf8d49e0bce5f4ee
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-09-24 11:32:52 +0300

    improved cards

--- src/CardContainerResult.js
+++ src/CardContainerResult.js
@@ -5,16 +5,12 @@ import { Row, Col } from 'react-bootstrap';
 
 class CardContainerResult extends React.Component {
     render() {
-        const items = [];
+        const items = [1,2,3,4,5,6];
+        const rows = items.map((item)=><Col className='d-flex justify-content-center' key={item} sx={12} sm={6} lg={4}><CardResult /></Col>)
 
         return (
             <Row>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
-                <Col className='d-flex justify-content-center' sx={12} sm={6} lg={4}><CardResult /></Col>
+               {rows}
             </Row>
         );
     }
--- src/CardResult.js
+++ src/CardResult.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faCalendarAlt, faUsers, faHeart, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
 
 
 class CardDisplaySearchResult extends React.Component {
@@ -9,9 +9,15 @@ class CardDisplaySearchResult extends React.Component {
         const title = 'C# Tutorial - Full Course for Beginners';
         const description = "This course will give you a full introduction into all of the core concepts in C# (aka C Sharp). Follow along with the course and you'll be a C# programmer in no ...";
         const channelTitle = "freeCodeCamp.org"
+        const publishedAt = new Date();
+        const views = 19999;
+        const likes = 1200;
+        const disLikes = 200;
+        const urlImg = "https://i.ytimg.com/vi/GhQdlIFylQ8/default.jpg";
+
         return (
             <Card style={{ width: '18rem' }} className="mb-3 mt-3">
-                <Card.Img variant="top" src="https://i.ytimg.com/vi/GhQdlIFylQ8/default.jpg" />
+                <Card.Img variant="top" src={urlImg} />
                 <Card.Body>
                     <Card.Title className='text-center'>{title}</Card.Title>
                     <Card.Text>
@@ -20,6 +26,30 @@ class CardDisplaySearchResult extends React.Component {
                     <Card.Text className='text-center text-muted'>
                         <small><FontAwesomeIcon icon={faUser} /> {channelTitle}</small>
                     </Card.Text>
+                    <Row>
+                        <Col sx={6} sm={6}>
+                            <Card.Text className='text-muted'>
+                                <small><FontAwesomeIcon icon={faCalendarAlt} /> {publishedAt.toLocaleDateString()}</small>
+                            </Card.Text>
+                        </Col>
+                        <Col sx={6} sm={6}>
+                            <Card.Text className='text-muted'>
+                                <small><FontAwesomeIcon icon={faUsers} /> {views}</small>
+                            </Card.Text>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sx={6} sm={6}>
+                            <Card.Text className='text-muted'>
+                                <small><FontAwesomeIcon icon={faHeart} /> {likes}</small>
+                            </Card.Text>
+                        </Col>
+                        <Col sx={6} sm={6}>
+                            <Card.Text className='text-muted'>
+                                <small><FontAwesomeIcon icon={faThumbsDown} /> {disLikes}</small>
+                            </Card.Text>
+                        </Col>
+                    </Row>
                 </Card.Body>
             </Card>
         );
