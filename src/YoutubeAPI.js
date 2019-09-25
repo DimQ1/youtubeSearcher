7a96728e6b1cbd39ac302081efa3318ed589a6bf
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-09-25 10:52:26 +0300

    updated query builder in YoutubeApi

--- src/YoutubeAPI.js
+++ src/YoutubeAPI.js
@@ -4,7 +4,7 @@ class youtubeAPI {
         this._type = 'video';
         //this._key = key ? key : 'AIzaSyBMe0YAZiT9xnr2Zl7N-cCT3b15S0OLNkE';
         //this._key = key ? key : "AIzaSyAwYHaDBzo1_I1ZuCnhBxjJN_tL-EjQuZ4";
-        this._key = key ? key :'AIzaSyCunpeDH93FQRFR33zHRnsfuATLjQSSzks';
+        this._key = key ? key : 'AIzaSyCunpeDH93FQRFR33zHRnsfuATLjQSSzks';
         this._maxResults = 6;
 
         this._url = new URL('https://www.googleapis.com/youtube/v3/search');
@@ -14,27 +14,30 @@ class youtubeAPI {
         search_params.append('key', this._key);
         search_params.append('maxResults', this._maxResults);
 
-        this._url.search=search_params;
+        this._url.search = search_params;
         this.onLoadData = onLoadData;
     }
 
     getUrlWithQuery = (query, nextPageToken) => {
         const search_params = new URLSearchParams(this._url.search);
-        search_params.set('q', query);
-        if(nextPageToken){
+        if (query) {
+            search_params.set('q', query);
+        }
+
+        if (nextPageToken) {
             search_params.set('pageToken', nextPageToken);
         }
 
         const url = this._url;
-        url.search=search_params;
+        url.search = search_params;
 
-        return query ? url.toString() : this._url.toString();
+        return url.toString();
     }
 
     handleLoadData = (xhr) => {
-        this.onLoadData(xhr.target.response);
-        // update the state of the component with the result here
-        // this.setState({ snippetsFormYoutube: this.response, nextPageToken: this.response.nextPageToken, prevPageToken: this.response.prevPageToken });
+        if (this.onLoadData) {
+            this.onLoadData(xhr.target.response);
+        }
     }
 
     getData = (query, nextPageToken) => {
@@ -43,15 +46,14 @@ class youtubeAPI {
 
     searchRequest = (urlWithQuery) => {
         // create a new XMLHttpRequest
-        var xhr = new XMLHttpRequest()
-
+        var xhr = new XMLHttpRequest();
         // get a callback when the server responds
-        xhr.addEventListener('load', this.handleLoadData)
+        xhr.addEventListener('load', this.handleLoadData);
         // open the request with the verb and the url
         xhr.open('GET', urlWithQuery);
         xhr.responseType = "json";
         // send the request
-        xhr.send()
+        xhr.send();
     }
 }
 
