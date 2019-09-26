e33742d9b65919af1c85df23d399ddff50bc35dc
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-09-26 11:08:56 +0300

    Fixed issue

--- src/Search.js
+++ src/Search.js
@@ -9,29 +9,25 @@ class Search extends React.Component {
         this.props.onSubmit(event.target.elements[0].value);
     }
 
-    handleChange = (event)=>{
+    handleChange = (event) => {
         this.props.onChange(event.target.value);
     }
 
     render() {
         const query = this.props.query ? this.props.query : '';
         return (
-            <Row>
-                <Col sm={12}>
-                    <form onSubmit={this.handleSubmit}>
-                        <InputGroup className="mt-3 mb-3">
-                            <FormControl value={query} onChange={this.handleChange}
-                                placeholder="Input search query"
-                                aria-label="Input search query"
-                                aria-describedby="basic-addon2"
-                            />
-                            <InputGroup.Append>
-                                <Button variant="outline-secondary" type="submit"><FontAwesomeIcon icon={faSearch} ></FontAwesomeIcon></Button>
-                            </InputGroup.Append>
-                        </InputGroup>
-                    </form>
-                </Col>
-            </Row>
+            <form onSubmit={this.handleSubmit}>
+                <InputGroup className="mt-3 mb-3">
+                    <FormControl value={query} onChange={this.handleChange}
+                        placeholder="Input search query"
+                        aria-label="Input search query"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary" type="submit"><FontAwesomeIcon icon={faSearch} ></FontAwesomeIcon></Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </form>
         );
     }
 }
